494f4b7950b1b7e942e75b06d02cb394cbe425d9
Zhang_Xu <axhuoga1203@163.com>
2025-03-09 22:33:17 +0800

    Create GuessNumberGame.java

---
+++ GuessNumberGame.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessNumberGame {
+    public static void main(String[] args) {
+        // 创建随机数生成器
+        Random random = new Random();
+        // 随机生成一个1到100
